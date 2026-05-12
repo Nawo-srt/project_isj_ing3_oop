beb33b06ef4155d71ad588912933807d3e525de3
Nawo-srt <ange.taks23@gmail.com>
2026-05-12 22:02:08 +0100

    Update Equipements.py

--- Equipements.py
+++ Equipements.py
@@ -1,5 +1,5 @@
 class Equipement :
-    """Modélise un équipement réseau"""
+    """Modélise un équipement réseau : chaque équipement a un nom, une marque, une adresse IP et un état (actif ou inactif)"""
     
     def __init__ (self, nom, marque, adresse_ip) :
         self.nom = nom
@@ -9,7 +9,6 @@ class Equipement :
     
     def activer (self) :
         self.est_actif = True
-        # print(f"{self.nom} est activé !")
     
     def desactiver (self) :
         self.est_actif = False
@@ -32,6 +31,7 @@ class Equipement :
         print(f"  Statut : {statut}")
 
 class Routeur(Equipement):
+    """Un routeur est un équipement qui a des interfaces réseau et une table de routage pour acheminer les paquets vers les réseaux appropriés."""
     
     def __init__(self, nom, marque, adresse_ip, nb_interfaces):
         super().__init__(nom, marque, adresse_ip)
@@ -47,7 +47,7 @@ class Routeur(Equipement):
         print(f"  Table de routage : {self.table_routage}")
 
 class Switch(Equipement):
-    
+    """Un switch est un équipement qui connecte plusieurs appareils sur un réseau local (LAN) et permet la communication entre eux en utilisant des adresses MAC pour acheminer les données vers les ports appropriés."""
     def __init__(self, nom, marque, adresse_ip, nb_ports) :
         super().__init__(nom, marque, adresse_ip)
         self.nb_ports = nb_ports
@@ -62,7 +62,7 @@ class Switch(Equipement):
         print(f"  VLAN actifs : {self.vlan_actifs}")
         
 class Serveur(Equipement):
-    
+    """Un serveur est un équipement qui fournit des services ou des ressources à d'autres appareils sur le réseau, tels que l'hébergement de sites web, la gestion de bases de données ou la fourniture de services de messagerie."""
     def __init__(self, nom, marque, adresse_ip, ram_go, cpu_coeurs) :
         super().__init__(nom, marque, adresse_ip)
         self.ram_go = ram_go
@@ -80,7 +80,7 @@ class Serveur(Equipement):
         print(f"  Services pris en charge : {self.services}")
 
 class Firewalls(Equipement):
-
+    """Un firewall est un équipement de sécurité réseau qui contrôle le trafic entrant et sortant en appliquant des règles de filtrage."""
     def __init__(self, nom, marque, adresse_ip, regle_filtrage) :
         super().__init__(nom, marque, adresse_ip)
         self.regle_filtrage = []
@@ -93,7 +93,7 @@ class Firewalls(Equipement):
         print(f"   Regles de filtrage : {self.regle_filtrage}")
 
 class PointAccesWifi(Equipement):
-
+    """Un point d'accès Wi-Fi est un équipement qui permet aux appareils de se connecter à un réseau sans fil (Wi-Fi) en fournissant une connexion sans fil à Internet ou à un réseau local."""
     def __init__(self, nom, marque, adresse_ip,wifi_connecte):
         super().__init__(nom, marque, adresse_ip)
         self.wifi_connecte = wifi_connecte
@@ -103,7 +103,7 @@ class PointAccesWifi(Equipement):
         print(f"   Wifi connecte : {self.wifi_connecte}")
 
 class Client(Equipement):
-
+    """Un client est un équipement connecté, un terminal à un réseau et qui utilise les services fournis par d'autres équipements."""
     def __init__(self, nom, marque, adresse_ip, passerelle):
         super().__init__(nom, marque, adresse_ip)
         self.passerelle = passerelle
